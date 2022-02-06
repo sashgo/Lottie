3749ff9b0ac171a088f607a5b860e78abc31f1fc
Oleksandr Bobel <alexander.bobel@techfabric.com>
2022-02-06 16:23:30 +0200

    Init project

---
+++ LottieFirst/MyPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LottieFirst
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        void animationView_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
